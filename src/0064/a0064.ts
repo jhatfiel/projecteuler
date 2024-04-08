0ca1149b3079dc092c400f75f3e7f7251e06d253
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-04-08 13:14:12 +0000

    Trying (and failing) to write a general cf calculator

--- src/0064/a0064.ts
+++ src/0064/a0064.ts
@@ -16,12 +16,15 @@ export class a0064 extends Puzzle {
         let sqrt = Math.floor(Math.sqrt(N));
         if (sqrt**2 !== N) { // skip perfect squares
             //this.log(`[${this.stepNumber.toString().padStart(5)}] n=${N} ${sqrt}`);
-            let cfRep = new ContinuedFraction(N, sqrt, 1).getRepeatingRepresentation();
-            //this.log(`[SQRT(${N})]=${ContinuedFraction.toString(cfRep)} period=${cfRep.len}`)
+            let cfRep = new RepeatingContinuedFraction(N, sqrt, 1).getRepeatingRepresentation();
+            let scf = new SimpleContinuedFraction(Math.sqrt(N));
+            //this.log(`[SQRT(${N})]=${RepeatingContinuedFraction.toString(cfRep)} period=${cfRep.len}`);
+            //this.log(`[SQRT(${N})]=${scf.toString()}`);
             if (cfRep.len % 2 === 1) {
                 //this.log(`[SQRT(${N})]=${ContinuedFraction.toString(cfRep)} period=${cfRep.len}`)
                 this.count++;
             }
+            //if (scf.period !== cfRep.len) moreToDo = false;
         }
 
         if (!moreToDo) {
@@ -31,8 +34,35 @@ export class a0064 extends Puzzle {
     }
 }
 
+// this doesn't work because decimal precision isn't good enough...
+class SimpleContinuedFraction {
+    digits: number[] = [];
+    period = NaN;
+    constructor(public num: number, public maxDigits: number=50) {
+        let x = num;
+        let seen = new Map<string, number>();
+        while (Number.isFinite(x) && this.digits.length < maxDigits) {
+            let a = Math.floor(x);
+            let e = x-a;
+            let key = `${a},${e.toFixed(5)}`;
+            //console.log(`x=${x},a,e=(${key})`);
+            if (seen.has(key)) {
+                this.period = this.digits.length-1 - seen.get(key);
+                break;
+            }
+            this.digits.push(a);
+            seen.set(key, this.digits.length-2);
+            x = 1/e;
+        }
+    }
+
+    toString(): string {
+        return `[${this.digits[0]};${this.digits.slice(1)}] ${this.period?'Period '+this.period:''}`;
+    }
+}
+
 // This is a very very specialized ContinuedFraction class - it works for sqrt calculations and nothing else
-class ContinuedFraction {
+class RepeatingContinuedFraction {
     constructor(public base: number, public int: number, public num: number, public denDiff: number = int, public approx: number = int) {}
 
     getRepeatingRepresentation(): {digits: number[], len: number} {
@@ -41,13 +71,16 @@ class ContinuedFraction {
         let seen = new Map<string, number>();
 
         //console.log(`${this.debug()} START`);
-        while (this.int < 2*this.approx) {//!seen.has(this.partialToString())) {
+        digits.push(this.int);
+        while (!seen.has(this.partialToString())) {
             len++;
-            digits.push(this.int);
             seen.set(this.partialToString(), seen.size);
             this.next();
+            digits.push(this.int);
             //console.log(`${this.debug()}`);
         }
+        len--;
+        digits.pop();
         //len = seen.size - seen.get(this.partialToString());
 
         return {digits, len};
@@ -68,6 +101,6 @@ class ContinuedFraction {
     partialToString(): string {return `${this.base},${this.int},${this.num},${this.denDiff}`; }
 
     static toString(rep: {digits: number[], len: number}): string {
-        return `[${rep.digits[0]};${rep.digits.slice(1,rep.digits.length-rep.len)}${(rep.digits.length-rep.len>1)?',':''}${rep.digits.slice(rep.digits.length-rep.len)}]`;
+        return `[${rep.digits[0]};${rep.digits.slice(1,rep.digits.length-rep.len)}${(rep.digits.length-rep.len>1)?',':''}(${rep.digits.slice(rep.digits.length-rep.len)})]`;
     }
 }
